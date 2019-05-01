658d2bd79f7dd3c877cb0e51ad276976a84a249a
AZGHOUR-SAAD <saad.azghour@gmail.com>
2019-05-01 14:12:15 +0100

    my first React Component

---
+++ src/Hello.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import './Hello.css'
+
+
+class Hello extends Component {
+    render() {
+        return (
+            <div className="f1 tc">
+                <h1>Welcom to React</h1>
+                <p>{ this.props.greeting }</p>
+            </div>
+        );
+    }
+}
+
+
+// let Hello = (props) => {                 // think React, just a Function
+//     return (
+//         <div className="f1 tc">
+//             <h1>Welcom to React</h1>
+//             <p>{ props.greeting }</p>
+//         </div>
+//     );
+// }
+
+
+export default Hello
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Hello from './Hello';
+import 'tachyons';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Hello greeting = {"Hello Azghour-Saad"}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
